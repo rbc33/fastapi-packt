dde0fffc4233338cb43390235d5f7032d8468adb
rbc33 <ricardobenthem@gmail.com>
2026-04-13 21:20:45 +0200

    mod11

--- app/api/routers/seller.py
+++ app/api/routers/seller.py
@@ -1,12 +1,15 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form, Request
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.templating import Jinja2Templates
+from pydantic import EmailStr
 
 from app.api.dependencies import SellerServiceDep, get_seller_access_token
 from app.api.schemas.seller import SellerCreate, SellerRead
 from app.database.redis import add_jti_to_blacklist
-
+from app.utils import TEMPLATE_DIR
+from app.config import app_settings
 
 router = APIRouter(prefix="/seller", tags=["Seller"])
 
@@ -41,6 +44,49 @@ async def verify_seller_email(
 ):
     await service.verify_email(token)
     return {"detail": "Email verified successfully"}
+
+
+
+# Email password reset
+@router.get("/forgot_password")
+async def forgot_password(
+    email: EmailStr,
+    service: SellerServiceDep,
+):
+    await service.send_password_reset_link(email, router.prefix)
+
+    return {"detail": "Password reset email sent"}
+
+## Password reset form
+@router.get("/reset_password_form")
+async def get_reset_password_form(request: Request,token:str):
+    templates = Jinja2Templates(TEMPLATE_DIR)
+    return templates.TemplateResponse(
+        request=request,
+        name="password/reset.html",
+        context={
+            "reset_url": f"http://{app_settings.APP_DOMAIN}{router.prefix}/reset_password?token={token}",
+        },
+    )
+    pass
+
+
+# Reset Seller password
+@router.post("/reset_password")
+async def reset_password(
+    request: Request,
+    token: str,
+    password: Annotated[str, Form()],
+    service: SellerServiceDep,
+):
+    is_success = await service.reset_password(token, password)
+    templates = Jinja2Templates(TEMPLATE_DIR)
+
+
+    return templates.TemplateResponse(
+            request=request,
+            name="password/reset_success.html" if is_success else "password/reset_failed.html",
+        )   
 
 
 # Logout
--- app/services/user.py
+++ app/services/user.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from uuid import UUID
 
 import bcrypt
@@ -8,7 +9,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import SQLModel
 from app.services.base import BaseService
 from app.services.notification import NotificationService
-from app.utils import decode_url_safe_token, generate_access_token, generate_url_safe_token
+from app.utils import (
+    decode_url_safe_token,
+    generate_access_token,
+    generate_url_safe_token,
+)
 from app.config import app_settings
 
 
@@ -17,7 +22,9 @@ def hash_password(password: str) -> str:
 
 
 class UserService(BaseService):
-    def __init__(self, model: type[SQLModel], session: AsyncSession, tasks: BackgroundTasks):
+    def __init__(
+        self, model: type[SQLModel], session: AsyncSession, tasks: BackgroundTasks
+    ):
         self.model = model
         self.session = session
         self.notification_service = NotificationService(tasks)
@@ -45,7 +52,7 @@ class UserService(BaseService):
         )
 
         return user
-    
+
     async def verify_email(self, token: str):
         token_data = decode_url_safe_token(token)
 
@@ -89,3 +96,45 @@ class UserService(BaseService):
                 }
             }
         )
+
+    async def send_password_reset_link(self, email: str, router_prefix: str):
+        user = await self._get_by_email(email)
+
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="No user found with the given email",
+            )
+
+        token = generate_url_safe_token(
+            {
+                "id": str(user.id),
+            },
+            salt="password-reset",
+        )
+
+        await self.notification_service.send_email_with_template(
+            recipients=[user.email],
+            subject="Reset your password for fastship",
+            context={
+                "username": user.name,
+                "reset_url": f"http://{app_settings.APP_DOMAIN}{router_prefix}/reset_password_form?token={token}",
+            },
+            template_name="mail_password_reset.html",
+        )
+
+    async def reset_password(self, token: str, password: str):
+        token_data = decode_url_safe_token(
+            token,
+            salt="password-reset",
+            expiry=timedelta(days=1),
+        )
+        
+        if not token_data:
+            return False
+        
+        user = await self._get(UUID(token_data["id"]))
+        
+        user.password_hash = hash_password(password)
+        await self._update(user)
+        return True
